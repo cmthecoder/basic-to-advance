b07e4a9706d0bd64fd9d3a0c1863d3618bde9a74
Chris Mems <christopher.mems@.com>
2023-03-16 07:41:19 -0500

    practice with useContext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useState } from 'react';
 import './App.css';
-import Heading from './components/Heading';
+import Counter from './components/Counter';
+import MealsList from './components/MealsList';
+import MealsProvider from './provider/MealsProvider';
+
+
 
 
 function App() {
-  const [word, setWord] = useState('Eat')
-
-  const handleClick = () => {
-    setWord('Drink')
-  }
 
   return (
     <div className="App">
-      <Heading message={word + ' at Little Lemon'}/>
-      <button onClick={handleClick}>Click here</button>
+      <MealsProvider>
+        <MealsList />
+        <Counter />
+      </MealsProvider>
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useMealsListContext } from '../provider/MealsProvider'
+
+const Counter = () => {
+  const { meals } = useMealsListContext()
+
+  return (
+    <div>
+      <h3>Number of meals today: {meals.length}</h3>
+    </div>
+  )
+}
+
+export default Counter
